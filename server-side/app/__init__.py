bb004252def03d1d5b8ab34a11e621ea99873c10
jpg157 <jasonlau5000@gmail.com>
2026-01-17 21:32:14 -0800

    implemented google login/signup and logout with oauth, with redirection to the react client app

--- server-side/app/__init__.py
+++ server-side/app/__init__.py
@@ -14,12 +14,19 @@ db = SQLAlchemy(model_class=Base, app=app)
 migrate = Migrate(app, db)
 
 oauth = OAuth(app=app)
-oauth.register("nwHacks 2026",
-               )
+oauth.register("nwHacks_2026_app",
+  client_id=app.config.get("OAUTH2_CLIENT_ID"),
+  client_secret=app.config.get("OAUTH2_CLIENT_SECRET"),
+  server_metadata_url=app.config.get("OAUTH2_META_URL"),
+  client_kwargs={
+      "scope": "openid profile email",
+  }
+)
 
 socketio = SocketIO(app=app,
  logger=True,
  engineio_logger=True,
  cors_allowed_origins="*" # this is very dangerous but it's fineeeee
 )
-from app import api_routes, web_socket_routes, authentication_routes
+
+from app import api_routes, web_socket_routes, auth_routes
